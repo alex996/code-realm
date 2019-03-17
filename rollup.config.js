8e076d3fff169f30d4caf82fb3067d9eefac0fb6
Alex <alexnezhynsky@gmail.com>
2019-03-16 20:07:39 -0400

    [chore] Set up dev server

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import copy from 'rollup-plugin-copy-glob'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
 
 const dest = 'build'
@@ -11,7 +13,8 @@ export default {
   input: 'src/index.js',
   output: {
     file: `${dest}/bundle.js`,
-    format: 'iife'
+    format: 'iife',
+    sourcemap: true
   },
   plugins: [
     resolve(),
@@ -19,6 +22,17 @@ export default {
       exclude: 'node_modules/**'
     }),
     copy([{ files: 'index.html', dest }], { verbose: true, watch: dev }),
+    dev &&
+      serve({
+        open: true,
+        port: 3000,
+        contentBase: dest,
+        historyApiFallback: true
+      }),
+    dev &&
+      livereload({
+        watch: dest
+      }),
     prod && terser()
   ]
 }
