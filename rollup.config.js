e4703bf45fb244efba12799088c58c6b063ffb5e
Alex <alexnezhynsky@gmail.com>
2019-03-17 12:30:55 -0400

    [feat] Navbar brand & icons

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import json from 'rollup-plugin-json'
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import copy from 'rollup-plugin-copy-glob'
+import url from 'rollup-plugin-url'
 import postcss from 'rollup-plugin-postcss'
+import copy from 'rollup-plugin-copy-glob'
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
@@ -23,6 +24,9 @@ export default {
     resolve(),
     babel({
       exclude: 'node_modules/**'
+    }),
+    url({
+      limit: 256
     }),
     postcss({
       extract: `${dest}/style.css`,
--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,6 +1,36 @@
 import { h } from 'preact'
 import { useState } from 'preact/hooks'
+import logo from '../logo.png'
+import { YouTube, GitHub, Medium, StackOverflow, Minds } from '../icons'
 import { homepage, version } from '../../package.json'
+
+const links = [
+  {
+    href: 'https://www.youtube.com/c/CodeRealm',
+    icon: YouTube,
+    title: 'YouTube'
+  },
+  {
+    href: 'https://github.com/alex996',
+    icon: GitHub,
+    title: 'GitHub'
+  },
+  {
+    href: 'https://stackoverflow.com/users/5610777/alex',
+    icon: StackOverflow,
+    title: 'StackOverflow'
+  },
+  {
+    href: 'https://medium.freecodecamp.org/@coderealm',
+    icon: Medium,
+    title: 'Medium'
+  },
+  {
+    href: 'https://www.minds.com/CodeRealm',
+    icon: Minds,
+    title: 'Minds'
+  }
+]
 
 const Navbar = props => {
   const [open, setOpen] = useState(false)
@@ -17,14 +47,14 @@ const Navbar = props => {
       <div class='container'>
         <div class='navbar-brand'>
           <a class='navbar-item' href='#'>
-            <strong>Code Realm</strong>
+            <img src={logo} alt='Code Realm' />
           </a>
 
           <a
             role='button'
             class={`navbar-burger burger${active}`}
             aria-label='menu'
-            aria-expanded={open}
+            aria-expanded={`${open}`}
             onClick={handleToggle}
           >
             <span aria-hidden='true' />
@@ -35,21 +65,19 @@ const Navbar = props => {
 
         <div class={`navbar-menu${active}`}>
           <div class='navbar-start'>
-            <a class='navbar-item' href='#'>
-              YouTube
-            </a>
-
-            <a class='navbar-item' href='#'>
-              GitHub
-            </a>
-
-            <a class='navbar-item' href='#'>
-              StackOverflow
-            </a>
-
-            <a class='navbar-item' href='#'>
-              Minds
-            </a>
+            {links.map(({ href, icon, title }) => (
+              <a
+                href={href}
+                title={title}
+                target='_blank'
+                class='navbar-item is-flex'
+              >
+                <span class='icon'>
+                  <img src={`/${icon}`} alt={title} />
+                </span>
+                <span class='is-hidden-desktop'>{title}</span>
+              </a>
+            ))}
           </div>
 
           <div class='navbar-end'>
