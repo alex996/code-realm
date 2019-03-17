4e95e68e64a1dd9b91c947ac43d15c18cf232808
Alex <alexnezhynsky@gmail.com>
2019-03-16 20:00:21 -0400

    [chore] Add HTML template

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,24 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+import copy from 'rollup-plugin-copy-glob'
+import { terser } from 'rollup-plugin-terser'
 
-const build = 'build'
+const dest = 'build'
+const dev = process.env.ROLLUP_WATCH
+const prod = !dev
 
 export default {
   input: 'src/index.js',
   output: {
-    file: `${build}/bundle.js`,
+    file: `${dest}/bundle.js`,
     format: 'iife'
   },
   plugins: [
     resolve(),
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    copy([{ files: 'index.html', dest }], { verbose: true, watch: dev }),
+    prod && terser()
   ]
 }
