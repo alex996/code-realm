c0f5f82bcfe60f08df3f1e91786abaca9ab71a87
Alex <alexnezhynsky@gmail.com>
2019-03-28 19:44:20 -0400

    [refactor] Remove superfluous components to speed up rendering

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,8 @@
 import { h } from 'preact'
 import clsx from 'clsx'
 
-const Article = ({ class: className, children }) => (
-  <article class={clsx('post', className)}>{children}</article>
-)
-
-const Title = ({ class: className, children, slug, prefix = 'blog' }) => {
-  const title = <h3 class={clsx('title is-spaced', className)}>{children}</h3>
-
-  return slug ? <a href={`/${prefix}/${slug}`}>{title}</a> : title
-}
-
-const Subtitle = ({ class: className, children }) => (
-  <h4 class={clsx('subtitle', className)}>{children}</h4>
-)
-
-const Caption = ({ class: className, children }) => (
-  <div class={clsx('caption has-text-grey is-flex', className)}>{children}</div>
-)
-
 const Post = ({
+  prefix = 'blog',
   slug,
   title,
   subtitle,
@@ -27,32 +10,36 @@ const Post = ({
   minsToRead,
   tags,
   preview
-}) => (
-  <Article class={preview && 'post-preview'}>
-    <Title class={preview && 'is-size-4'} slug={preview && slug}>
-      {title}
-    </Title>
+}) => {
+  const heading = (
+    <h3 class={clsx('title is-spaced', preview && 'is-size-4')}>{title}</h3>
+  )
 
-    <Subtitle class={preview && 'is-size-6'}>{subtitle}</Subtitle>
+  return (
+    <article class={clsx('post', preview && 'post-preview')}>
+      {preview ? <a href={`/${prefix}/${slug}`}>{heading}</a> : heading}
 
-    <Caption class={preview && 'is-small'}>
-      <span class='item'>{createdAt}</span>
+      <h4 class={clsx('subtitle', preview && 'is-size-6')}>{subtitle}</h4>
 
-      <span class='separator'>&bull;</span>
+      <div class={clsx('caption has-text-grey is-flex', preview && 'is-small')}>
+        <span class='item'>{createdAt}</span>
 
-      <span class='item'>{minsToRead} mins</span>
+        <span class='separator'>&bull;</span>
 
-      <span class='separator'>&bull;</span>
+        <span class='item'>{minsToRead} mins</span>
 
-      <div class='item'>
-        <div class='tags'>
-          {tags.map(tag => (
-            <span class='tag is-rounded'>{tag}</span>
-          ))}
+        <span class='separator'>&bull;</span>
+
+        <div class='item'>
+          <div class='tags'>
+            {tags.map(tag => (
+              <span class='tag is-rounded'>{tag}</span>
+            ))}
+          </div>
         </div>
       </div>
-    </Caption>
-  </Article>
-)
+    </article>
+  )
+}
 
 export default Post
